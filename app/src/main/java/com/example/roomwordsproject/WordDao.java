cb6a1e3a0616c3637c2225282200bab7ffa92a2c
seamu.th2363 <seamu.th2363@vusd.us>
2023-01-19 15:27:36 -0800

    Initial commit

---
+++ app/src/main/java/com/example/roomwordsproject/WordDao.java
@@ -0,0 +1,21 @@
+package com.example.roomwordsproject;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM word_table ORDER BY word COLLATE NOCASE ASC")
+    List<Word> getAllWords();
+}
